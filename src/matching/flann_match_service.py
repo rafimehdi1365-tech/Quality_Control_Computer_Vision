3c73f100d6afd9a8f48eeaa0c2f5ce4e7162d08c
MehdiRafiaee <me.rafiaee65@gmail.com>
2025-10-10 11:53:03 +0330

    Update flann_match_service.py

--- src/matching/flann_match_service.py
+++ src/matching/flann_match_service.py
@@ -1,16 +1,32 @@
 import cv2
-import numpy as np
 
-def flann_match(des1, des2):
-    FLANN_INDEX_KDTREE = 1
-    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
-    search_params = dict(checks=50)
+def run_flann_match(des1, des2, detector_name="SIFT"):
+    """
+    Run FLANN-based matching between two descriptor sets.
+    :param des1: descriptors from image 1
+    :param des2: descriptors from image 2
+    :param detector_name: name of the feature detector
+    :return: good matches (ratio test)
+    """
+    if detector_name in ["ORB", "BRISK"]:
+        # For binary descriptors
+        index_params = dict(algorithm=6,  # FLANN_INDEX_LSH
+                            table_number=6,
+                            key_size=12,
+                            multi_probe_level=1)
+        search_params = dict(checks=50)
+    else:
+        # For float descriptors (SIFT, AKAZE)
+        index_params = dict(algorithm=1, trees=5)  # FLANN_INDEX_KDTREE
+        search_params = dict(checks=50)
 
     flann = cv2.FlannBasedMatcher(index_params, search_params)
     matches = flann.knnMatch(des1, des2, k=2)
 
-    good = []
+    # Lowe’s ratio test
+    good_matches = []
     for m, n in matches:
-        if m.distance < 0.7 * n.distance:
-            good.append(m)
-    return good
+        if m.distance < 0.75 * n.distance:
+            good_matches.append(m)
+
+    return sorted(good_matches, key=lambda x: x.distance)
